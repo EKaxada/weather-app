c0074679d8e889d16234367fbed9aa57169086ee
EKaxada <enocksdha@gmail.com>
2021-03-01 16:16:15 +0300

    Updates

---
+++ precache-manifest.68dce12dfb5b0e41aa4fb992e2b7c629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669717a2616a9e658d5de22f4b3ee90b",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "434a049bdf1e76f15d93",
+    "url": "/weather-app/static/css/main.637261d8.chunk.css"
+  },
+  {
+    "revision": "cd54978e824efe42fa23",
+    "url": "/weather-app/static/js/2.84d40489.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.84d40489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434a049bdf1e76f15d93",
+    "url": "/weather-app/static/js/main.863d1719.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "0fa5880177fed0cc55f0a3a742d36c52",
+    "url": "/weather-app/static/media/back-image-2.0fa58801.jpg"
+  }
+]);
